eef4a1b7bbf86315ae496b267610e85e6dcfcf0b
eborio <contacto@todoprogramacion.com.ve>
2014-09-09 18:27:12 -0430

    Pequeños cambios: acentuaciones, puntos y algunas palabras para que el mensaje sea más acorde al lenguaje.

--- upload_lang.php
+++ upload_lang.php
@@ -1,12 +1,12 @@
 <?php
 
 $lang['upload_userfile_not_set'] = "No se ha podido encontrar una variable de tipo POST llamada userfile.";
-$lang['upload_file_exceeds_limit'] = "El archivo subido excede el tamaño máximo permitido en la configuración de su PHP";
-$lang['upload_file_partial'] = "El archivo fue subido parcialmente";
-$lang['upload_no_file_selected'] = "No ha seleccionado ningún archivo para subir";
-$lang['upload_invalid_filetype'] = "El tipo de archivo que esta tratando de subir no está permitido";
-$lang['upload_invalid_filesize'] = "El archivo que está tratando de subir es más largo que el tamaño permitido";
-$lang['upload_invalid_dimensions'] = "La imagen que esta tratando de subir excede los tamaños máximos para el alto y/o ancho";
+$lang['upload_file_exceeds_limit'] = "El archivo subido excede el tamaño máximo permitido en la configuración de su PHP.";
+$lang['upload_file_partial'] = "El archivo fue subido parcialmente.";
+$lang['upload_no_file_selected'] = "No ha seleccionado ningún archivo para subir.";
+$lang['upload_invalid_filetype'] = "El tipo de archivo que esta tratando de subir no está permitido.";
+$lang['upload_invalid_filesize'] = "El archivo que está tratando de subir es más grande que el tamaño permitido.";
+$lang['upload_invalid_dimensions'] = "La imagen que está tratando de subir excede los tamaños máximos para el alto y/o ancho.";
 $lang['upload_destination_error'] = "Se encontraron problemas mientras se trataba de mover el archivo subido al directorio de destino final.";
 $lang['upload_no_filepath'] = "La dirección para subir el archivo parece no ser válida.";
 $lang['upload_no_file_types'] = "Todavía no ha especificado ningún archivo permitido.";

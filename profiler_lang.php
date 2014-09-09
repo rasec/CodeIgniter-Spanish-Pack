637905df90277b95e380bbcbee4fecafc24f7c0a
eborio <contacto@todoprogramacion.com.ve>
2014-09-09 18:29:50 -0430

    Traducidas algunas palabras que aparecían en inglés.

--- profiler_lang.php
+++ profiler_lang.php
@@ -1,15 +1,15 @@
 <?php
 
-$lang['profiler_database']		= 'DATABASE';
-$lang['profiler_controller_info'] = 'CLASS/METHOD';
+$lang['profiler_database']		= 'BASE DE DATOS';
+$lang['profiler_controller_info'] = 'CLASE/MÉTODO';
 $lang['profiler_benchmarks']	= 'BENCHMARKS';
-$lang['profiler_queries']		= 'QUERIES';
-$lang['profiler_get_data']		= 'GET DATA';
-$lang['profiler_post_data']		= 'POST DATA';
-$lang['profiler_uri_string']	= 'URI STRING';
-$lang['profiler_memory_usage']	= 'MEMORY USAGE';
-$lang['profiler_config']		= 'CONFIG VARIABLES';
-$lang['profiler_headers']		= 'HTTP HEADERS';
+$lang['profiler_queries']		= 'CONSULTAS';
+$lang['profiler_get_data']		= 'DATOS GET';
+$lang['profiler_post_data']		= 'DATOS POST';
+$lang['profiler_uri_string']	= 'CADENA URI';
+$lang['profiler_memory_usage']	= 'USO DE MEMRIA';
+$lang['profiler_config']		= 'VARIABLES DE CONFIGURACIÓN';
+$lang['profiler_headers']		= 'CABECERAS HTTP';
 $lang['profiler_no_db']			= 'El driver para Base de Datos no ha sido cargado';
 $lang['profiler_no_queries']	= 'No se han ejecutado consultas';
 $lang['profiler_no_post']		= 'No existen datos de tipo POST';
@@ -18,8 +18,8 @@ $lang['profiler_no_uri']		= 'No existen datos de tipo URI';
 $lang['profiler_no_memory']		= 'Uso de memoria no disponible';
 $lang['profiler_no_profiles']	= 'No hay datos de perfil - se desabilitan todas las secciones del perfil.';
 /* Added after 2.0.2 */
-$lang['profiler_session_data']	= 'SESSION DATA';
-$lang['profiler_section_hide']	= 'Esconder';
+$lang['profiler_session_data']	= 'DATOS DE SESIÓN';
+$lang['profiler_section_hide']	= 'Ocultar';
 $lang['profiler_section_show']	= 'Mostrar';
 
 /* End of file profiler_lang.php */
